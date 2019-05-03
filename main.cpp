7208692887293a14250321f71fc9594416ff8386
vna15 <30414490+vna15@users.noreply.github.com>
2019-05-02 22:23:49 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include "voxel_sculptor.h"
+#include "sculptor.cpp"
 
 using namespace std;
 
 int main()
 {
-    Sculptor paint(10,10,10);
-    paint.setColor(1,1,1,1);
-    paint.putBox(1,1,1,1,1,1);
-    paint.cutBox(1,1,1,1,1,1);
-    paint.putSphere(1,1,1,1);
-    paint.cutSphere(1,1,1,1);
-    paint.putEllipsoid(1,1,1,1,1,1);
-    paint.cutEllipsoid(1,1,1,1,1,1);
+    Sculptor paint(2,2,2);
+    //paint.setColor(1,1,1,1);
+    //paint.putBox(1,1,1,1,1,1);
+    //paint.cutBox(1,1,1,1,1,1);
+    //paint.putSphere(1,1,1,1);
+    //paint.cutSphere(1,1,1,1);
+    //paint.putEllipsoid(1,1,1,1,1,1);
+    //paint.cutEllipsoid(1,1,1,1,1,1);
+    paint.writeOFF("C:/Users/vnaze/Desktop/teste.off");
 
     return 0;
-}
+}   
+    
+    
